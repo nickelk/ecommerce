e8ef7ffa5160053e3505efeb8a7868226d4ca6a5
nickelk <nick_smith@ukr.net>
2021-11-23 12:45:30 +0200

    db connected

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommercedb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',  # 127.0.0.1
+        'PORT': '5432',
     }
 }
 
